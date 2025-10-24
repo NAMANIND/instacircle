dd2798fd0c4548c06b7b47f8e2011f919fea5ed3
Naman rai <namanrai309@gmail.com>
2025-10-24 23:39:05 +0530

    nhj

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@prisma/client'],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      const {
-        PrismaPlugin,
-      } = require('@prisma/nextjs-monorepo-workaround-plugin');
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
-    return config;
-  },
-  turbopack: {},
 };
 
 export default nextConfig;
